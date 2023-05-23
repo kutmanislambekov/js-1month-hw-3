5f9b627d13bea0978f8a68b795db83637ce04d93
kutmanislambekov <kutmanislambekov2007@gmail.com>
2023-05-23 20:13:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+
+ let number = prompt('Введите число')
+ function isNegative(num) {
+    let result = ''
+    if(num === null){
+        result = 'вы нажали на отмену'
+    }else if(num === ""){
+        result = 'вы нажали на ok'
+    }else if(isNaN(num)){
+       result="введите число!"
+   }else if(num > 0){
+    result = `число ${num} положительное`
+   }else{
+    result = `число ${num} отрицательное`
+   }
+   return result
+ }
+
+ 
+ alert(isNegative(number))
+
+// доп дз
+
+
+ let count = prompt('Введите число')
+ let g = 0
+ for(let i = 0; i < count; i++){
+    switch(g){
+        case 0:{
+            document.write("<div class='block red'></div>")
+            g++
+            break
+        }
+        case 1:{
+            document.write("<div class='block yellow'></div>")
+            g++
+            break
+        }
+        default:{
+            document.write("<div class='block green'></div>")
+            g = 0
+          
+        }
+    }
+
+ }
+
+//  for(let i = 0; i < 5; i++){
+//     console.log(i);
+//  }
+
+//  let i = 0
+
+//  i = i  + 1
